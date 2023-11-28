792a122cc97d6d72f17b507ec6ed062aaa25eccd
Vivekanandan97 <148924164+Vivekanandan97@users.noreply.github.com>
2023-11-29 01:45:47 +0530

    Fulladder.v

--- Day3/Fulladder.v
+++ Day3/Fulladder.v
@@ -1,6 +1,6 @@
 module fulladder(a,b,Cin,sum,carry);
 input a,b,Cin;
 output sum,carry;
-assign diff = a ^ b ^ Cin;
-assign barrow = (a & b) | (b & Cin) | (Cin & a); 
+assign sum = a ^ b ^ Cin;
+assign carry = (a & b) | (b & Cin) | (Cin & a); 
 endmodule
