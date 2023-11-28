29d76ce4be1b61d616236a078fa935b18ce6f301
Vivekanandan97 <148924164+Vivekanandan97@users.noreply.github.com>
2023-11-28 23:34:03 +0530

    Fulladder_tb.v

--- Day3/Fulladder_tb.v
+++ Day3/Fulladder_tb.v
@@ -1,1 +1,17 @@
-
+module fulladder_tb();
+reg a,b,Cin;
+wire sum,carry;
+fullsubractor i1(.a(a),.b(b),.Cin(Cin),.sum(sum),.carry(carry));
+initial
+begin
+a = 1'b0 ; b = 1'b0; Cin = 1'b0;#5
+a = 1'b0 ; b = 1'b0; Cin = 1'b1;#5
+a = 1'b0 ; b = 1'b1; Cin = 1'b0;#5
+a = 1'b0 ; b = 1'b1; Cin = 1'b1;#5
+a = 1'b1 ; b = 1'b0; Cin = 1'b0;#5
+a = 1'b1 ; b = 1'b0; Cin = 1'b1;#5
+a = 1'b1 ; b = 1'b1; Cin = 1'b0;#5
+a = 1'b1 ; b = 1'b1; Cin = 1'b1;#5
+$stop;
+end
+endmodule
